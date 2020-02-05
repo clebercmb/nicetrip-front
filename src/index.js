3609df33082606b862a5fcf82a2ad3af2b209e0c
Cleber Miranda Barbosa <cleber@gmail.com>
2020-02-05 08:15:46 -0300

    Fixing conflicts

--- src/index.js
+++ src/index.js
@@ -6,17 +6,9 @@ import Layout from "./layout"
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 
-<<<<<<< HEAD
-ReactDOM.render(<App />, document.getElementById('root'));
-||||||| merged common ancestors
-ReactDOM.render(<App />, document.getElementById('root'));
-
-=======
-import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.render(<Layout />, document.getElementById('root'));
 
->>>>>>> 6228be1c0e22300b4a722c78afa03d98a25dd7f6
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
